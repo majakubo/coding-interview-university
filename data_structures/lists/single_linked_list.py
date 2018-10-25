51460fcf89649a579e7e6d5b7ce7544788cdafc4
majaku <m.jakubowski4git@gmail.com>
2018-10-26 00:29:11 +0200

    implemented list, without reverse and remove value

--- data_structures/lists/single_linked_list.py
+++ data_structures/lists/single_linked_list.py
@@ -1,42 +1,141 @@
+from .node import Node
+
+
 class SingleLinkedList:
-    def __init__(self):
-        pass
+    def __init__(self, sequence=None):
+        self.length = 0
+        self.head = None
+        self.tail = None
+        if sequence:
+            for element in sequence:
+                self.push_front(element)
 
     def size(self):
-        pass
+        """returns number of data elements in list"""
+        return self.length
 
     def empty(self):
-        pass
+        """bool returns true if empty"""
+        return True if self.length == 0 else False
 
     def value_at(self, index):
-        pass
+        return self.node_at(index).data
+
+    def node_at(self, index):
+        """returns the value of the nth item (startig at 0 for first)"""
+        if index >= self.length:
+            raise IndexError("To large index")
+
+        current_node = self.head
+
+        for i in range(index):
+            current_node = current_node.next
+
+        return current_node
 
     def push_front(self, value):
-        pass
+        """adds an item to the front of the list"""
+        if self.length == 0:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            buffer = self.head
+            self.head = Node(value)
+            self.head.next = buffer
 
-    def pop_front(self, value):
-        pass
+        self.length += 1
 
-    def push_back(self):
-        pass
+    def pop_front(self):
+        """remove front item from front and return its value"""
+        if self.length == 0:
+            raise NotImplementedError("no implementation for popping from empty list")
+
+        buffer = self.head
+        self.head = self.head.next
+
+        self.length -= 1
+
+        if self.length == 0:
+            self.tail = None
+            self.head = None
+
+        return buffer.data
+
+    def push_back(self, value):
+        """adds an item at the end"""
+        if self.length == 0:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            buffer = self.tail
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+
+        self.length += 1
+
+    def pop_back(self):
+        """removes item from end of lits and return its value"""
+        if self.length == 0:
+            raise NotImplementedError("no implementation for popping from empty list")
+
+        last = self.tail.data
+        penultimate = self.node_at(self.length - 2)
+        self.tail = penultimate
+
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return last
 
     def front(self):
-        pass
+        """get value of front item"""
+        return self.head.data
 
     def back(self):
-        pass
+        """get value of end item"""
+        return self.head.tail
 
     def insert(self, index, value):
-        pass
+        """insert value at index"""
+        if index > self.length:
+            raise NotImplementedError("There is no implementation "
+                                      "for inserting outside a list")
+        if index == 0:
+            self.push_front(value)
+        elif index == self.length - 1:
+            self.push_back(value)
+        else:
+            before_index = self.node_at(index - 1)
+            buffer = Node(value)
+            buffer.next = before_index.next
+            before_index.next = buffer
+            self.length += 1
 
     def erase(self, index):
-        pass
+        """removes node at given index"""
+        if index >= self.length:
+            raise IndexError("index out of bounds")
+
+        if index == 0:
+            self.pop_front()
+        elif index == self.length - 1:
+            self.pop_back()
+        else:
+            before_index = self.node_at(index - 1)
+            before_index.next = before_index.next.next
+            self.length -= 1
 
     def value_n_from_end(self, n):
-        pass
+        """returns the value of the node at nth position from the end of the list"""
+        return self.value_at(self.length - n - 1)
 
     def reverse(self):
+        """reverses the list"""
         pass
 
-    def remove_value(self):
+    def remove_value(self, value):
+        """removes first node that has value equal to given one"""
         pass
--- data_structures/lists/test_linked_list.py
+++ data_structures/lists/test_linked_list.py
@@ -1,7 +1,234 @@
-import unittest
-import single_linked_list
+from .single_linked_list import SingleLinkedList
 
 
-class SingleLinkedListTest(unittest.TestCase):
-    def test_list(self):
-        self.assertEqual(1, 1)
+def test_push_front():
+    l = SingleLinkedList()
+    l.push_front(1)
+    assert l.head.data == 1
+    assert l.tail.data == 1
+
+    l.push_front(2)
+    assert l.head.data == 2
+    assert l.tail.data == 1
+    assert l.head.next.data, l.tail.data
+
+    l.push_front(3)
+    assert l.head.data == 3
+    assert l.head.next.data == 2
+
+
+def test_push_back():
+    l = SingleLinkedList()
+    l.push_back(1)
+    assert l.tail.data == 1
+    assert l.head.data == 1
+
+    l.push_back(2)
+    assert l.tail.data == 2
+    assert l.head.data == 1
+    assert l.head.next.data == l.tail.data
+
+    l.push_back(3)
+    assert l.tail.data == 3
+    assert l.head.next.next.data == 3
+
+
+def test_push_back_second():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    assert l.tail.data == 4
+    assert l.head.data == 0
+
+
+def test_value_at():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    assert l.value_at(0) == 0
+    assert l.value_at(2) == 2
+    assert l.value_at(4) == 4
+
+
+def test_pop_front():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    for i in range(4):
+        assert l.pop_front() == i
+        assert l.head.data == i + 1
+
+    assert l.head.data == l.tail.data
+    assert l.tail.data == 4
+
+    l.pop_front()
+
+    assert l.head == None
+    assert l.tail == None
+
+
+def test_pop_back():
+    l = SingleLinkedList()
+
+    l.push_back(1)
+    assert l.head.data == 1
+    assert l.tail.data == 1
+
+    l.push_back(2)
+    assert l.head.data == 1
+    assert l.tail.data == 2
+
+    l.push_back(3)
+    assert l.head.data == 1
+    assert l.tail.data == 3
+
+    assert l.pop_back(), 3
+    assert l.pop_back(), 2
+    assert l.pop_back(), 1
+
+
+def test_front():
+    l = SingleLinkedList()
+    for i in range(10):
+        l.push_back(i)
+        assert 0 == l.front()
+
+    l = SingleLinkedList()
+    for i in range(10):
+        l.push_front(i)
+        assert i == l.front()
+
+
+def test_back():
+    l = SingleLinkedList()
+    for i in range(10):
+        l.push_back(i)
+
+    assert 9 == l.tail.data
+
+
+def test_instert_head():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    assert l.head.data == 0
+    assert l.tail.data == 4
+
+    l.insert(0, 33)
+    assert l.head.data == 33
+
+
+def test_insert_tail():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    l.insert(4, 33)
+
+    assert l.tail.data == 33
+    assert l.length == 6
+
+
+def test_insert_middle():
+    l = SingleLinkedList()
+
+    for i in range(10):
+        l.push_back(i)
+
+    l.insert(3, 99)
+
+    assert l.head.next.next.next.data == 99
+    assert l.head.next.next.next.next.data == 3
+
+
+def test_erase_head():
+    l = SingleLinkedList()
+    l.push_front(1)
+    assert l.head.data == 1
+
+    l.erase(0)
+    assert l.head == None
+
+
+def test_erase_tail():
+    l = SingleLinkedList()
+    l.push_back(1)
+    assert l.tail.data == 1
+
+    l.push_back(2)
+    assert l.tail.data == 2
+
+    l.push_back(3)
+    assert l.tail.data == 3
+
+    l.erase(2)
+
+    assert l.tail.data == 2
+
+
+def test_erase_middle():
+    l = SingleLinkedList()
+    l.push_back(1)
+    l.push_back(2)
+    l.push_back(3)
+
+    assert l.head.next.data == 2
+    l.erase(1)
+    assert l.head.next.data == 3
+
+
+def test_value_n_from_end():
+    l = SingleLinkedList()
+    for i in range(10):
+        l.push_back(i)
+
+    assert l.value_n_from_end(0) == 9
+    assert l.value_n_from_end(3) == 6
+
+
+def test_reverse():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_front(i)
+
+    l.reverse()
+
+    assert l.head.data == 0
+    assert l.tail.data == 5
+
+    for i in range(5):
+        assert l.value_at(i), i
+
+
+def test_remove_value_front():
+    l = SingleLinkedList
+    for i in range(5):
+        l.push_back(i)
+
+    l.remove_value(0)
+
+    assert l.head.data == 2
+
+
+def test_remove_value_middle():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    l.remove_value(1)
+
+    assert l.head.next == 3
+
+
+def test_remove_value_back():
+    l = SingleLinkedList()
+    for i in range(5):
+        l.push_back(i)
+
+    l.remove_value(4)
+
+    assert l.tail.data == 3
