63c3b7a779a8e62877a1bdfae5cdb8501f1c1cc4
majaku <m.jakubowski4git@gmail.com>
2018-10-25 17:35:45 +0200

    created dir for lists and skeleton for single linked list

---
+++ data_structures/lists/single_linked_list.py
@@ -0,0 +1,42 @@
+class SingleLinkedList:
+    def __init__(self):
+        pass
+
+    def size(self):
+        pass
+
+    def empty(self):
+        pass
+
+    def value_at(self, index):
+        pass
+
+    def push_front(self, value):
+        pass
+
+    def pop_front(self, value):
+        pass
+
+    def push_back(self):
+        pass
+
+    def front(self):
+        pass
+
+    def back(self):
+        pass
+
+    def insert(self, index, value):
+        pass
+
+    def erase(self, index):
+        pass
+
+    def value_n_from_end(self, n):
+        pass
+
+    def reverse(self):
+        pass
+
+    def remove_value(self):
+        pass
---
+++ data_structures/lists/test_linked_list.py
@@ -0,0 +1,7 @@
+import unittest
+import single_linked_list
+
+
+class SingleLinkedListTest(unittest.TestCase):
+    def test_list(self):
+        self.assertEqual(1, 1)
