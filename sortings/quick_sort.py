cb41f0d8b0464edbb28c2875a6e7f627b9f7ee26
majaku <m.jakubowski4git@gmail.com>
2018-11-01 10:29:51 +0100

    commit tostage actual data in remote repository

--- sortings/quick_sort.py
+++ sortings/quick_sort.py
@@ -1,1 +1,40 @@
-"""not implemented yet"""
+def quick_sort(seq, first=None, last=None):
+    if first == None and last == None:
+        first = 0
+        last = len(seq) - 1
+
+    if first < last:
+        split = partition(seq, first, last)
+
+        quick_sort(seq, first, split - 1)
+        quick_sort(seq, split + 1, last)
+
+
+def partition(seq, first, last):
+    pivot = seq[first]
+
+    left = first + 1
+    right = last
+
+    done = False
+    while not done:
+
+        while left <= right and seq[left] <= pivot:
+            left = left + 1
+
+        while seq[right] >= pivot and right >= left:
+            right = right - 1
+
+        if right < left:
+            done = True
+        else:
+            seq[left], seq[right] = seq[right], seq[left]
+
+    seq[left], seq[right] = seq[right], seq[left]
+
+    return right
+
+
+seq = list(range(10, 0, -1))
+quick_sort(seq)
+print(seq)
