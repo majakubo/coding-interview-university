cba6d073c6559306f78b66a9d9e7b00ca387e00a
majaku <m.jakubowski.git@gmail.com>
2018-11-29 18:04:23 +0100

    implemented merge sort

--- sortings/merge_sort.py
+++ sortings/merge_sort.py
@@ -1,1 +1,21 @@
-"""not implemented yet"""
+def merge_sort(sequence):
+    """simple merge sort on additional lists"""
+    if len(sequence) > 1:
+        middle = len(sequence) // 2
+        left = merge_sort_dumb(sequence[:middle])
+        right = merge_sort_dumb(sequence[middle:])
+        merged = []
+        while left and right:
+            if left < right:
+                merged.append(left.pop(0))
+            else:
+                merged.append(right.pop(0))
+        if left:
+            merged += left
+        if right:
+            merged += right
+
+        return merged
+    else:
+        return sequence
+
